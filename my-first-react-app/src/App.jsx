4aa911efce8185536178b649ece602d2fa8d282c
Tanvideshmukh14 <tanvidesh8@mail.com>
2026-02-06 12:10:01 +0530

    transforming list in js using map()

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,57 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//class components
-//eg. class ClassComponet extends React.Component{
-// render(){
-// return <h2>Class Component</h2>
-//  }
-// }
-
-//
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// function App(){
-// }
-const Card = ({title}) =>{
-  return(
-    <h2>{title}</h2>
-  )
+const data = [
+  {
+  id:"1",
+  title : "Tiramisu",
+  description: "The best tiramisu in town",
+  image: "https://picsum.photos/200/300/?random",
+  price:"$5.00",
 }
+,
+{
+  id:"2",
+  title : "Lemon ice Cream",
+  description: "Mind blowing taste",
+  image: "https://picsum.photos/200/300/?random",
+  price:"$4.50",
+},
+{
+  id:"3",
+  title : "Chocolate mousse",
+  description: "Unexplored flavors",
+  image: "https://picsum.photos/200/300/?random",
+  price:"$6.00",
+},
+
+];
+const topDesserts = data.map(dessert => {
+  return{
+    content: `${dessert.title}-${dessert.description}`,
+    price: dessert.price, 
+  }
+})
 const App = () => {
+  console.log(topDesserts);
   return(
     <>
-    <h2>Functional Arrow Component</h2>
-    <Card title="StarWars" rating={5} isCool={true} actors={[{name : 'Actors'},{name : 'Actors'}]}/>
-    <Card title="Avatar"/>
-    <Card title="The Lion King"/>
     </>
   )
 }
